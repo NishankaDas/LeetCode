af96c776ea4ad7933f0fde9d055590286d54bfcb
Nishanka Das <137529239+NishankaDas@users.noreply.github.com>
2025-09-21 09:53:29 +0530

    Create prob9_palindrom.py

---
+++ prob9_palindrom.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if(x<0):
+            return False
+        x = str(x)
+        y = x[::-1]
+        if (x==y):
+            return True
+        else:
+            return False
+        
